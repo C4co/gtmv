0895b9db0b15386e6009d2f085d31e789e60a34a
Carlos Costa <carllos.nc@gmail.com>
2023-05-17 23:50:27 -0300

    creating Formater and Results classes

--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@ using Pastel;
 using Sharprompt;
 using View;
 
-class Runner
+class Formater
 {
-    static String FormatMovieOption(Movie movie)
+    public static String FormatMovieOption(Movie movie)
     {
         return $"{Txt.Yellow(movie.year.ToString())} | {Txt.Cyan(movie.title)} - {movie.id}";
     }
 
-    static String FormatTorrentOption(Torrent torrent, int index)
+    public static String FormatTorrentOption(Torrent torrent, int index)
     {
         String title = $"[YTS] Torrent {torrent.quality}";
         String seedPeers = $"Seeds: {torrent.seeds} - Peers: {torrent.peers}";
@@ -21,7 +21,7 @@ class Runner
         return $"{index} | {Txt.Cyan(title)} | {torrent.size} - {torrent.type} | {Txt.Yellow(seedPeers)}";
     }
 
-    static String FormatSubtitleOption(Subtitle subtitle, int index)
+    public static String FormatSubtitleOption(Subtitle subtitle, int index)
     {
         var colorRating = Color.Black;
         var bakcgroundColorRating = Color.White;
@@ -37,54 +37,46 @@ class Runner
 
         String rating = $" ♥ {subtitle.rating} ".Pastel(colorRating).PastelBg(bakcgroundColorRating);
 
-        return $"{rating} | {index} | {subtitle.language} ";
+        return $"{rating} | {index} | {subtitle.language}";
     }
+}
 
-    //show just movie result
-    static void ShowMovieResult(Movie movie)
+class Results
+{
+    public static void ShowMovieResult(Movie movie)
     {
         Console.WriteLine();
-
         Console.WriteLine($"{Txt.White("▬▬▬▬▬ MOVIE ▬▬▬▬▬")}");
-
         Console.WriteLine();
-
         Console.WriteLine($"{Txt.Yellow(movie.year.ToString())} - {Txt.Cyan(movie.title)}");
         Console.WriteLine($"Genres: {Txt.Green(String.Join(", ", movie.genres))}");
         Console.WriteLine($"Language: {Txt.Green(movie.language)}");
         Console.WriteLine($"Cover: {Txt.Green(movie.large_cover_image!)}");
         Console.WriteLine($"Trailer: {Txt.Green("https://www.youtube.com/watch?v=" + movie.yt_trailer_code)}");
-
         Console.WriteLine();
     }
 
-    static void ShowTorrentResult(Torrent torrent)
+    public static void ShowTorrentResult(Torrent torrent)
     {
         Console.WriteLine($"{Txt.White("▬▬▬▬▬ TORRENT ▬▬▬▬▬")}");
-
         Console.WriteLine();
-
         Console.WriteLine($"{Txt.CyanBg($" [YTS] Torrent file {torrent.quality} ")}: {Txt.Green(torrent.url)}");
-
         Console.WriteLine();
-
         Console.WriteLine($"{Txt.CyanBg($" Magnet link {torrent.quality} ")}: {Txt.Green(torrent.getMagnetLink()!)}");
-
         Console.WriteLine();
     }
 
-
-    static void ShowSubtitleResult(Subtitle subtitle)
+    public static void ShowSubtitleResult(Subtitle subtitle)
     {
         Console.WriteLine($"{Txt.White("▬▬▬▬▬ SUBTITLE ▬▬▬▬▬")}");
-
         Console.WriteLine();
-
         Console.WriteLine($"{Txt.CyanBg($" Subtitle {subtitle.language} ")}: {Txt.Green(subtitle.downloadLink!)}");
-
         Console.WriteLine();
     }
+}
 
+class Runner
+{
     static public async Task Run()
     {
         HttpClient httpClient = new HttpClient();
@@ -116,11 +108,11 @@ class Runner
             });
         }
 
-        var movieSelected = Prompt.Select($"Select movie: {movies?.data?.movie_count} results", movies?.data?.movies.Select(FormatMovieOption).ToList());
+        var movieSelected = Prompt.Select($"Select movie: {movies?.data?.movie_count} results", movies?.data?.movies.Select(Formater.FormatMovieOption).ToList());
         var movieId = movieSelected.Split(" - ")[1];
         var movieFiltered = movies?.data?.movies.Where(movie => movie.id.ToString() == movieId).ToList().First();
 
-        var torrentSelected = Prompt.Select("Select torrent", movieFiltered?.torrents.Select(FormatTorrentOption).ToList());
+        var torrentSelected = Prompt.Select("Select torrent", movieFiltered?.torrents.Select(Formater.FormatTorrentOption).ToList());
         var torrentIndex = torrentSelected.Split(" | ")[0];
         var torrentFiltered = movieFiltered?.torrents.Where((torrent, index) => index.ToString() == torrentIndex).ToList().First();
 
@@ -138,17 +130,17 @@ class Runner
 
         if (subtitles.Count != 0)
         {
-            var subtitleSelected = Prompt.Select("Select subtitle", subtitles.Select(FormatSubtitleOption).ToList());
+            var subtitleSelected = Prompt.Select("Select subtitle", subtitles.Select(Formater.FormatSubtitleOption).ToList());
             var subtitleIndex = subtitleSelected.Split(" | ")[1];
             var subtitleFiltered = subtitles.Where((subtitle, index) => index.ToString() == subtitleIndex).ToList().First();
-            ShowMovieResult(movieFiltered!);
-            ShowTorrentResult(torrentFiltered!);
-            ShowSubtitleResult(subtitleFiltered!);
+            Results.ShowMovieResult(movieFiltered!);
+            Results.ShowTorrentResult(torrentFiltered!);
+            Results.ShowSubtitleResult(subtitleFiltered!);
         }
         else
         {
-            ShowMovieResult(movieFiltered!);
-            ShowTorrentResult(torrentFiltered!);
+            Results.ShowMovieResult(movieFiltered!);
+            Results.ShowTorrentResult(torrentFiltered!);
         }
     }
 }
